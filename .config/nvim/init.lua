9a4cf09bfbcded3743a9afc9cae14b1de4e96525
UnladenCoconut <benjkollar03@gmail.com>
2026-02-14 00:15:53 +0000

    wofi, neovim, wezterm configs

---
+++ .config/nvim/init.lua
@@ -0,0 +1,6 @@
+require("config.lazy")
+
+vim.opttabstop=2 
+vim.opt.shiftwidth=2
+vim.opt.softtabstop=2
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/neodev.nvim",
+  {  
+    "mason-org/mason.nvim",
+    opts = {
+      PATH = "append",
+    }
+  },
+  "folke/trouble.nvim",
+}
